b9434ee06f5a38433cd8639fa3b44f306790eaa2
Ben Strathdee <ben.strathdee@live.com.au>
2021-12-07 09:07:10 +1100

    added early structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import styles from "./App.module.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getProducts, seedProducts } from "./services/products";
+import Navbar from "./containers/Navbar";
+import Home from "./containers/Home";
+import ProductPage from "./containers/ProductPage";
 
 function App() {
-	return <div className={styles.App}></div>;
+	const [products, setProducts] = useState([]);
+
+	const getData = async () => {
+		const data = await getProducts();
+		setProducts(data);
+	};
+
+	useEffect(() => {
+		getData();
+		console.log(products);
+	}, []);
+
+	return (
+		<Router>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/:productID" element={<ProductPage />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
