29697a7de103f95d802f9553ab9c44e2aa0bd0e0
Ben Strathdee <ben.strathdee@live.com.au>
2021-12-11 22:31:57 +1100

    added structure/content/styling

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,7 +1,51 @@
 import styles from "./ProductCard.module.scss";
+import { Link } from "react-router-dom";
+import { useEffect, useContext } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { updateProduct } from "../../services/products";
+import { DataContext } from "../../context/DataContext/DataContext";
 
-const ProductCard = () => {
-	return <div></div>;
+const ProductCard = ({ product }) => {
+	const { getData } = useContext(DataContext);
+	const iconClass = product.isFav
+		? styles.ProductCard_Heart__Fav
+		: styles.ProductCard_Heart;
+
+	const handleClick = async () => {
+		product.isFav = !product.isFav;
+		const { id, ...record } = product;
+		await updateProduct(id, record, "products");
+		console.log(product.isFav);
+	};
+
+	useEffect(() => {
+		getData();
+	}, [product]);
+
+	return (
+		<div className={styles.ProductCard}>
+			<Link to={`product/${product.id}`}>
+				<img
+					src={product.ref_image}
+					alt=""
+					className={styles.ProductCard_Image}
+				/>
+			</Link>
+			<h5 className={styles.ProductCard_Name}>
+				{product.name}
+				<FontAwesomeIcon
+					icon={faHeart}
+					className={iconClass}
+					onClick={handleClick}
+				/>
+			</h5>
+			<p className={styles.ProductCard_Subtitle}>{product.subtitle}</p>
+			<h4 className={styles.ProductCard_Price}>
+				${product.price_per_unit}
+			</h4>
+		</div>
+	);
 };
 
 export default ProductCard;
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,40 @@
+import { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import { SearchContext } from "../../context/SearchContext/SearchContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
 import styles from "./Search.module.scss";
 
 const Search = () => {
-	return <div></div>;
+	const { setSearch } = useContext(SearchContext);
+	const [searchTerm, setSearchTerm] = useState("");
+
+	const handleChange = (event) => {
+		setSearchTerm(event.target.value);
+	};
+
+	const handleClick = () => {
+		setSearch(searchTerm);
+	};
+
+	return (
+		<div className={styles.Search}>
+			<input
+				type="text"
+				onChange={handleChange}
+				className={styles.Search_Searchbar}
+			/>
+			<Link to="/">
+				<button
+					onClick={handleClick}
+					className={styles.Search_SearchBtn}
+				>
+					<FontAwesomeIcon icon={faSearch} />
+				</button>
+			</Link>
+		</div>
+	);
 };
 
 export default Search;
