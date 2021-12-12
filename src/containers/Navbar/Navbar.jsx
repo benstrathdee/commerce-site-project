96640523865e8287d2ea5f40e572cf729d969ee4
Ben Strathdee <ben.strathdee@live.com.au>
2021-12-12 22:05:35 +1100

    added cart information, styling

--- src/containers/Navbar/Navbar.jsx
+++ src/containers/Navbar/Navbar.jsx
@@ -3,8 +3,11 @@ import Search from "./../../components/Search";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { DataContext } from "../../context/DataContext/DataContext";
+import { useContext } from "react";
 
 const Navbar = () => {
+	const { cartItems } = useContext(DataContext);
 	return (
 		<div className={styles.Navbar}>
 			<Link to="/" className={styles.Navbar_Link}>
@@ -13,10 +16,26 @@ const Navbar = () => {
 				</div>
 			</Link>
 			<Search />
-			<FontAwesomeIcon
-				icon={faShoppingCart}
-				className={styles.Navbar_Cart}
-			/>
+			<Link to="cart" className={styles.CartWrapper}>
+				<FontAwesomeIcon
+					icon={faShoppingCart}
+					className={styles.CartWrapper_Cart}
+				/>
+				<div className={styles.CartWrapper_Units}>
+					{cartItems.reduce((totalUnits, item) => {
+						return (totalUnits += parseInt(item.units));
+					}, 0)}
+				</div>
+				<div className={styles.CartWrapper_Total}>
+					{cartItems.length > 0
+						? "$" +
+						  cartItems.reduce((price, item) => {
+								return (price +=
+									item.units * item.price_per_unit);
+						  }, 0)
+						: null}
+				</div>
+			</Link>
 		</div>
 	);
 };
