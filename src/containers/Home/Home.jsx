dc4688eb6b010a1d68b7dc05e2cb12a8ef010568
Ben Strathdee <ben.strathdee@live.com.au>
2021-12-12 22:04:47 +1100

    fixed useEffect

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,14 +1,11 @@
 import styles from "./Home.module.scss";
 import ProductList from "./../../containers/ProductList";
 import FeaturedCarousel from "./../../containers/FeaturedCarousel";
-import { DataContext } from "../../context/DataContext/DataContext";
-import { useEffect, useContext } from "react";
+import { useEffect } from "react";
 
 const Home = () => {
-	const { getData } = useContext(DataContext);
 	useEffect(() => {
 		window.scrollTo(0, 0);
-		getData();
 	}, []);
 	return (
 		<div className={styles.Home}>
