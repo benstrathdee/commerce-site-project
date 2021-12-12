3d8ecfa11198d4542290d027fe4009b03373bfbe
Ben Strathdee <ben.strathdee@live.com.au>
2021-12-12 21:59:23 +1100

    added cart, moved navbar into dataprovider

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import styles from "./App.module.scss";
 import Navbar from "./containers/Navbar";
 import Home from "./containers/Home";
 import ProductPage from "./containers/ProductPage";
+import Cart from "./containers/Cart/Cart";
 import { SearchProvider } from "./context/SearchContext/SearchContext";
 import { DataProvider } from "./context/DataContext/DataContext";
 
@@ -11,10 +12,11 @@ function App() {
 		<div className={styles.App}>
 			<SearchProvider>
 				<Router>
-					<Navbar />
 					<DataProvider>
+						<Navbar />
 						<Routes>
 							<Route path="/" element={<Home />} />
+							<Route path="cart" element={<Cart />} />
 							<Route
 								path="product/:id"
 								element={<ProductPage />}
