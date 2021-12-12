30bc693a6e22df08324e3bdcceef4906d65df803
Ben Strathdee <ben.strathdee@live.com.au>
2021-12-12 22:03:59 +1100

    added structure, functionality

--- src/containers/Cart/Cart.jsx
+++ src/containers/Cart/Cart.jsx
@@ -1,7 +1,40 @@
-import styles from "./Card.module.scss";
+import styles from "./Cart.module.scss";
+import { useContext, useEffect } from "react";
+import { DataContext } from "../../context/DataContext/DataContext";
+import CartItem from "../../components/CartItem/CartItem";
 
 const Cart = () => {
-	return <div></div>;
+	const { cartItems } = useContext(DataContext);
+
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [cartItems]);
+
+	return (
+		<div className={styles.Cart}>
+			{cartItems.length > 0 ? (
+				<>
+					{cartItems.map((item, index) => {
+						return <CartItem item={item} key={index} />;
+					})}
+					<div className={styles.TotalWrapper}>
+						<div className={styles.TotalWrapper_Total}>
+							Total: $
+							{cartItems.reduce((price, item) => {
+								return (price +=
+									item.units * item.price_per_unit);
+							}, 0)}
+						</div>
+						<button className={styles.TotalWrapper_Checkout}>
+							Proceed to checkout
+						</button>
+					</div>
+				</>
+			) : (
+				<p>Your cart is empty.</p>
+			)}
+		</div>
+	);
 };
 
 export default Cart;
